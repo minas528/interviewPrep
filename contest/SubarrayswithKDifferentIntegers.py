724abdebd4bb0ce7631fb7befc44c5f239d9ff31
Minas <54796851+minas528@users.noreply.github.com>
2020-12-04 16:14:39 +0300

    Create SubarrayswithKDifferentIntegers.py

---
+++ contest/SubarrayswithKDifferentIntegers.py
@@ -0,0 +1,40 @@
+https://leetcode.com/problems/subarrays-with-k-different-integers/submissions/
+992. Subarrays with K Different Integers
+Difficulty: Hard
+
+class Solution:
+    def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
+        
+        return (self.atMostK(A, len(A), K) 
+                - self.atMostK(A, len(A), K - 1))
+    
+    def atMostK(self, arr, n, k):
+        count = 0
+        left = 0
+        right = 0
+
+        cache_map = {}
+
+        while(right < n):
+
+            if arr[right] not in cache_map:
+                cache_map[arr[right]] = 0
+
+            cache_map[arr[right]] += 1
+
+            while(len(cache_map) > k):
+
+                if arr[left] not in cache_map:
+                    cache_map[arr[left]] = 0
+
+                cache_map[arr[left]] -= 1
+
+                if cache_map[arr[left]] == 0:
+                    del cache_map[arr[left]]
+
+                left += 1
+
+            count += right - left + 1
+            right += 1
+
+        return count
