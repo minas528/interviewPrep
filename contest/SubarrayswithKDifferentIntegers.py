9e1324d154bcb4fd29da6a146f534c0694d61e58
Minas <54796851+minas528@users.noreply.github.com>
2020-12-04 16:19:44 +0300

    Update SubarrayswithKDifferentIntegers.py

--- contest/SubarrayswithKDifferentIntegers.py
+++ contest/SubarrayswithKDifferentIntegers.py
@@ -1,6 +1,8 @@
+'''
 https://leetcode.com/problems/subarrays-with-k-different-integers/submissions/
 992. Subarrays with K Different Integers
 Difficulty: Hard
+'''
 
 class Solution:
     def subarraysWithKDistinct(self, A: List[int], K: int) -> int:
