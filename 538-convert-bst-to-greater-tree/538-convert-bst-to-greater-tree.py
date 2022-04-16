ff83dfbd56373ea8e35b2f201c47791ee41cd4d9
Minasie Alemu <54796851+minas528@users.noreply.github.com>
2022-04-16 22:53:02 +0300

    Time: 87 ms (84.26%), Space: 16.8 MB (28.97%) - LeetHub

---
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def get_successor(node):
+            succ = node.right
+            while succ.left is not None and succ.left is not node:
+                succ = succ.left
+            return succ
+                
+        total = 0
+        node = root
+        while node is not None:
+            if node.right is None:
+                total += node.val
+                node.val = total
+                node = node.left
+            else:
+                succ = get_successor(node)
+                if succ.left is None:
+                    succ.left = node
+                    node = node.right
+                else:
+                    succ.left = None
+                    total += node.val
+                    node.val = total
+                    node = node.left
+        
+        return root
